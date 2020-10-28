b5a107d930d28edf518579f29fbe6b783fe7270b
Sandro <sandro.michl@web.de>
2020-10-28 17:22:10 +0100

    Commit Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        String text = "Hello World";
+        System.out.println(helloWorld(text));
+    }
+
+    private static String helloWorld(String text){
+        return text;
+    }
+}
